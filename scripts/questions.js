05a84e673d8cc7399bd4bab7c00c54be61c802ab
Cristina Calderon <calderocristina@gmail.com>
2022-12-02 17:05:56 -0700

    added in more files and code

---
+++ scripts/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        title: "How do we change something?",
+        choices: ["strings", "booleans", "alerts", "event"],
+        answer: "event",
+    },
+    {
+        title: "When was REACT created?",
+        choices: ["May 29, 2013", "January 5, 1999", "December 16, 2005", "September 26, 1988"],
+        answer: "May 29, 20013",
+    },
+    {
+        title: "How created REACT?",
+        choices: ["Twitter", " Snapchat", "Youtube", "Facebook"],
+        answer: "Facebook",
+    },
+    {
+        title: "What is REACT?.",
+        choices: ["React is a JavaScript-based UI development library", "It is a standard markup language for web page creation", "Extension to the JavaScript language syntax "],
+        answer: "React is a JavaScript-based UI development library",
+    },
+];
